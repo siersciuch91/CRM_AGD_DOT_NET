d0c48ad22aef7526bb83686bc1c7dd00d39e1d26
Adrian Grzegorek <siersciuch1991@gmail.com>
2018-10-04 18:07:40 +0200

    Add project files.

---
+++ CRM_AGD_DOT_NET/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CRM_AGD_DOT_NET.MvcApplication" Language="C#" %>
---
+++ CRM_AGD_DOT_NET/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CRM_AGD_DOT_NET.Startup))]
+namespace CRM_AGD_DOT_NET
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
